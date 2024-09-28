4f34e714bed0719b18568bb1124523e7b2d3741c
ガリレオだけ <151680308+itsjustgalileo@users.noreply.github.com>
2024-09-28 19:11:57 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "Downloading packages"
-sudo pacman -Syyu zsh tmux wget git github-cli pulseaudio alsa-utils jack2 libwebp xorg-server xorg-xinit xorg-xrandr xwallpaper xclip pcmanfm vifm clipmenu tree emacs llvm clang wine mingw-w64 lldb gdb cmake ninja python3 mesa mesa-utils valgrind libglvnd vulkan-icd-loader vulkan-intel vulkan-tools i3-wm i3status dmenu ttf-ibm-plex htop acpi unzip zip openssh rsync qemu-full libvirt virt-manager dnsmasq bridge-utils chromium picom nasm
+sudo pacman -Syyu zsh tmux wget git github-cli pulseaudio alsa-utils jack2 libwebp xorg-server xorg-xinit xorg-xrandr xwallpaper xclip pcmanfm vifm clipmenu tree vim emacs llvm clang wine mingw-w64 lldb gdb cmake ninja python3 mesa mesa-utils valgrind libglvnd vulkan-icd-loader vulkan-intel vulkan-tools i3-wm i3status dmenu ttf-ibm-plex htop acpi unzip zip openssh rsync qemu-full libvirt virt-manager dnsmasq bridge-utils chromium picom nasm
 
 echo "Setting the shell to zsh"
 chsh -s /bin/zsh
@@ -28,6 +28,21 @@ echo "Deploying dotfiles"
 # Make deploy script executable and run it
 chmod +x ~/code/dotfiles/deploy.sh
 ~/code/dotfiles/deploy.sh
+
+# Download vim setup https://github.com/amix/vimrc
+echo "Setting up VIM"
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+touch ~/.vim_runtime/my_configs.vim
+git clone https://github.com/widatama/vim-phoenix ~/.vim_runtime/my_plugins/vim-phoenix
+echo "set nu rnu" >> ~/.vim_runtime/my_configs.vim
+echo "colo phoenix" >> ~/.vim_runtime/my_configs.vim
+echo "PhoenixOrangeEighties" >> ~/.vim_runtime/my_configs.vim
+
+# Clone personal repos
+echo "Cloning bump repos"
+git clone https://github.com/itsjustgalileo/x-roulette ~/code/bump/x-roulette
+git clone https://github.com/itsjustgalileo/progen ~/code/bump/progen
 
 # Download NVM
 echo "Downloadding NVM"
