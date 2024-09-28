74a6699347f2207a7ec86f7527e022f7349b93aa
ガリレオだけ <151680308+itsjustgalileo@users.noreply.github.com>
2024-09-28 19:45:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "pacman: Downloading packages"
-sudo pacman -Syyu zsh tmux wget git github-cli pulseaudio alsa-utils jack2 libwebp xorg-server xorg-xinit xorg-xrandr xwallpaper xclip pcmanfm vifm clipmenu tree vim emacs llvm clang wine mingw-w64 lldb gdb cmake ninja python3 python-pip mesa mesa-utils valgrind libglvnd vulkan-icd-loader vulkan-intel vulkan-tools i3-wm i3status dmenu ttf-ibm-plex htop acpi unzip zip openssh rsync qemu-full libvirt virt-manager dnsmasq bridge-utils chromium picom nasm jdk11-openjdk rustc cargo go
+sudo pacman -Syyu zsh tmux wget git github-cli pulseaudio alsa-utils jack2 libwebp xorg-server xorg-xinit xorg-xrandr xwallpaper xclip pcmanfm vifm clipmenu tree vim emacs llvm clang wine mingw-w64-binutils mingw-w64-crt mingw-w64-gcc mingw-w64-headers mingw-w64-winpthreads lldb gdb cmake ninja python3 python-pip mesa mesa-utils valgrind libglvnd vulkan-icd-loader vulkan-intel vulkan-tools i3-wm i3status dmenu ttf-ibm-plex htop acpi unzip zip openssh rsync qemu-full libvirt virt-manager dnsmasq bridge-utils chromium picom nasm jdk11-openjdk rustup cargo go
 
 echo "zsh: Setting the shell to zsh"
 chsh -s /bin/zsh
