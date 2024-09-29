a69fb40e04b1e5d25915c114d835bdf4eb6f3fb8
ガリレオだけ <151680308+itsjustgalileo@users.noreply.github.com>
2024-09-28 21:22:07 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -44,10 +44,10 @@ sudo pacman-key --recv BC26F752D25B92CE272E0F44F7FD5492264BB9D0 --keyserver keys
 sudo pacman-key --lsign BC26F752D25B92CE272E0F44F7FD5492264BB9D0
 wget https://pkg.devkitpro.org/devkitpro-keyring.pkg.tar.xz
 sudo pacman -U devkitpro-keyring.pkg.tar.xz
-echo "[dkp-libs]" >> /etc/pacman.conf
-echo "Server = https://pkg.devkitpro.org/packages" >> /etc/pacman.conf
-echo "[dkp-linux]" >> /etc/pacman.conf
-echo "Server = https://pkg.devkitpro.org/packages/linux/$arch" >> /etc/pacman.conf
+echo "[dkp-libs]" | sudo tee -a /etc/pacman.conf
+echo "Server = https://pkg.devkitpro.org/packages" | sudo tee -a /etc/pacman.conf
+echo "[dkp-linux]" | sudo tee -a /etc/pacman.conf
+echo "Server = https://pkg.devkitpro.org/packages/linux/$arch" | sudo tee -a /etc/pacman.conf
 rm -rf devkitpro-keyring.pkg.tar.xz
 # System upgrade (optional: upgrade system packages and data)
 echo "Post-install: Upgrading system packages"
