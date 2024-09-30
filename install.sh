21608f7df608eed269123138cd85d36944c04518
ItsJustGalileo <itsjustgalileo@icloud.com>
2024-09-30 06:56:14 -0400

    added ada support and hoogle

---
+++ install.sh
@@ -0,0 +1,127 @@
+# \file install.sh
+# \brief Arch automated build script
+# This script download and installs all necessary
+# tools to setup my devenv on any freshly installed 
+# Arch Linux machine assuming that Linux was pacstraped
+# with the following packages:
+# pacstrap -K /mnt linux linux-firmware base base-devel networkmanager sof-firmware grub efibootmgr vi man-db man-pages zsh terminus-font
+# and the user was added with the flag `-s /bin/zsh`
+# some packages up here might not make sense in the
+# context of the setup, but they are just reminders to myself.
+#! /bin/sh
+
+set -e
+
+# Color definitions
+GREEN='\033[0;32m'     # Green
+ORANGE='\033[0;33m'    # Orange
+NC='\033[0m'           # No Color
+
+echo -e "${GREEN}[INFO] - pacman: Downloading packages${NC}"
+sudo pacman -Syyu tmux wget git github-cli diff-so-fancy git-lfs bat pulseaudio alsa-utils jack2 libwebp xorg-server xorg-xinit xorg-xrandr i3-wm i3status dmenu xwallpaper picom pcmanfm vifm clipmenu vim emacs llvm clang lldb gdb valgrind cmake ninja python3 python-pip ipython nasm jdk11-openjdk rustup go gcc-fortran gcc-ada hoogle wine wine-mono mingw-w64-binutils mingw-w64-crt mingw-w64-gcc mingw-w64-headers mingw-w64-winpthreads mesa mesa-utils libglvnd vulkan-icd-loader vulkan-intel vulkan-tools qemu-full libvirt virt-manager dnsmasq bridge-utils ttf-ibm-plex bpytop neofetch acpi unzip zip unrar arj p7zip ffmpeg openssh inetutils rsync mtools dosfstools xclip tree shellcheck chromium docker docker-compose
+
+# Black Arch
+echo -e "${GREEN}[INFO] - Running Black Arch Bootstrap${NC}"
+curl -o ~/strap.sh https://blackarch.org/strap.sh
+echo 26849980b35a42e6e192c6d9ed8c46f0d6d06047 ~/strap.sh | sha1sum -c
+chmod +x ~/strap.sh
+sudo ~/strap.sh
+rm -rf ~/strap.sh
+
+# DevKitPro setup
+echo -e "${GREEN}[INFO] - Post-install: Setting up DevKitPro${NC}"
+sudo pacman-key --recv BC26F752D25B92CE272E0F44F7FD5492264BB9D0 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign BC26F752D25B92CE272E0F44F7FD5492264BB9D0
+wget https://pkg.devkitpro.org/devkitpro-keyring.pkg.tar.xz
+sudo pacman -U devkitpro-keyring.pkg.tar.xz
+echo "[dkp-libs]" | sudo tee -a /etc/pacman.conf
+echo "Server = https://pkg.devkitpro.org/packages" | sudo tee -a /etc/pacman.conf
+echo "[dkp-linux]" | sudo tee -a /etc/pacman.conf
+echo "Server = https://pkg.devkitpro.org/packages/linux/\$arch" | sudo tee -a /etc/pacman.conf
+rm -rf devkitpro-keyring.pkg.tar.xz
+
+# System upgrade (optional: upgrade system packages and data)
+echo -e "${GREEN}[INFO] - Post-install: Upgrading system packages${NC}"
+sudo pacman -Syu
+
+sudo systemctl restart systemd-binfmt
+
+# Enable Docker and libvirtd services
+echo -e "${GREEN}[INFO] - Post-install: Enabling Docker and libvirt services${NC}"
+sudo systemctl enable --now docker
+sudo systemctl enable --now libvirtd
+
+echo -e "${GREEN}[INFO] - code: Making code directories${NC}"
+mkdir -p code/
+mkdir -p code/external
+
+echo -e "${GREEN}[INFO] - st: Downloading st${NC}"
+git clone https://git.suckless.org/st ~/code/external/st
+
+echo -e "${GREEN}[INFO] - st: Building st${NC}"
+cd ~/code/external/st
+sudo make clean install
+# Going back home
+cd ~
+
+# Downloading NVM for node and npm management
+echo -e "${GREEN}[INFO] - Downloading NVM for JS setup${NC}"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# Downloading Haskell tools
+echo -e "${GREEN}[INFO] - Downloading GHC up for Haskell setup${NC}"
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+
+# cc65 compiler toolchain for 65x cpu development
+echo -e "${GREEN}[INFO] - cc65: Downloading cc65 compiler toolchains${NC}"
+git clone https://github.com/cc65/cc65 ~/code/external/cc65
+
+echo -e "${GREEN}[INFO] - cc65: Building cc65${NC}"
+cd ~/code/external/cc65
+# building without install and not in sudo mode
+make
+# Going back home
+cd ~
+
+# dotfiles setup
+echo -e "${GREEN}[INFO] - st: Downloading dotfiles${NC}"
+git clone https://github.com/itsjustgalileo/dotfiles ~/code/dotfiles
+
+echo -e "${GREEN}[INFO] - dotfiles: Deploying dotfiles${NC}"
+chmod +x ~/code/dotfiles/deploy.sh
+~/code/dotfiles/deploy.sh
+
+# Setting up VIM
+echo -e "${GREEN}[INFO] - Setting up VIM${NC}"
+echo -e "${GREEN}[INFO] - VIM: Cloning Ultimate vimrc${NC}"
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo -e "${GREEN}[INFO] - VIM: Making custom config${NC}"
+touch ~/.vim_runtime/my_configs.vim
+echo -e "${GREEN}[INFO] - VIM: Downloading VIM phoenix theme${NC}"
+git clone https://github.com/widatama/vim-phoenix ~/.vim_runtime/my_plugins/vim-phoenix
+echo -e "${GREEN}[INFO] - VIM: Line number/relative line number${NC}"
+echo "set nu rnu" >> ~/.vim_runtime/my_configs.vim
+echo -e "${GREEN}[INFO] - VIM: Remapping keys${NC}"
+echo "inoremap jk <Esc>" >> ~/.vim_runtime/my_configs.vim
+echo -e "${GREEN}[INFO] - VIM: Setting up NerdTree${NC}"
+echo "let g:NerdTreeWinPos = 'left'" >> ~/.vim_runtime/my_configs.vim
+echo -e "${GREEN}[INFO] - VIM: Setting up colorscheme${NC}"
+echo "colo phoenix" >> ~/.vim_runtime/my_configs.vim
+echo "PhoenixBlueEighties" >> ~/.vim_runtime/my_configs.vim
+
+echo -e "${GREEN}[INFO] - Downloading post-install script${NC}"
+curl -o ~/post-install.sh https://raw.githubusercontent.com/itsjustgalileo/Arch-setup/master/post-install.sh
+chmod +x ~/post-install.sh
+
+echo -e "${ORANGE}[WARNING] - System about to reboot to apply changes${NC}"
+echo -e "${ORANGE}[WARNING] - Don't forget to run the post-install script after reboot by running: ${NC}"
+echo -e "${ORANGE}[WARNING] - ~/post-install.sh${NC}"
+
+echo "..."
+sleep 5
+
+rm -rf ~/setup.sh
+
+echo -e "${ORANGE}[WARNING] - Rebooting system...${NC}"
+reboot
--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,12 @@
+# \file post-install.sh
+# \brief post installation patches
 # This script should only exist on your fs
-# if it was downloaded via the setup.sh script
+# if it was downloaded via the install.sh script
 # Do not run it if it was not!
 #! /bin/sh
 
 # Color definitions
+GREEN='\033[0;31m'     # Red
 GREEN='\033[0;32m'     # Green
 ORANGE='\033[0;33m'    # Orange
 NC='\033[0m'           # No Color
@@ -11,88 +14,67 @@ NC='\033[0m'           # No Color
 # Ensure the script exits if any command fails
 set -e
 
-echo -e "${GREEN}Post-install: Setting up environment${NC}"
+echo -e "${GREEN}[INFO] - Post-install: Setting up environment${NC}"
 
 # Ensure NVM is loaded
 if [ -s "$HOME/.nvm/nvm.sh" ]; then
-    echo -e "${GREEN}Post-install: Loading NVM${NC}"
+    echo -e "${GREEN}[INFO] - Post-install: Loading NVM${NC}"
     . "$HOME/.nvm/nvm.sh"
 else
-    echo -e "${ORANGE}NVM not found, skipping NVM setup.${NC}"
+    echo -e "${RED}[WARNING] - NVM not found, skipping NVM setup.${NC}"
 fi
 
 # Install and use the latest stable Node.js via NVM
 if command -v nvm > /dev/null; then
-    echo -e "${GREEN}Post-install: Installing and using the latest Node.js with NVM${NC}"
+    echo -e "${GREEN}[INFO] - Post-install: Installing and using the latest Node.js with NVM${NC}"
     nvm install node
     nvm use node
 else
-    echo -e "${ORANGE}NVM command not found, skipping Node.js setup.${NC}"
+    echo -e "${RED}[WARNING] - NVM command not found, skipping Node.js setup.${NC}"
 fi
+
+# Installing Bun JS (Just in case)
+echo -e "Post-install: Installing BunJS"
+curl -fsSL https://bun.sh/install | bash
+
+# Installing Deno JS (Just in case)
+echo -e "Post-install: Installing DenoJS"
+curl -fsSL https://deno.land/install.sh | sh
 
 # Update Rust and set the stable toolchain
 if command -v rustup > /dev/null; then
-    echo -e "${GREEN}Post-install: Updating Rust toolchain to the latest stable version${NC}"
+    echo -e "${GREEN}[INFO] - Post-install: Updating Rust toolchain to the latest stable version${NC}"
     rustup update stable
     rustup self upgrade-data
 else
-    echo -e "${ORANGE}Rustup not found, skipping Rust update.${NC}"
+    echo -e "${RED}[WARNING] - Rustup not found, skipping Rust update.${NC}"
 fi
 
 # Log in to GitHub CLI
 if command -v gh > /dev/null; then
-    echo -e "${GREEN}Post-install: Logging into GitHub CLI${NC}"
+    echo -e "${GREEN}[INFO] - Post-install: Logging into GitHub CLI${NC}"
     gh auth login
 else
-    echo -e "${ORANGE}GitHub CLI (gh) not found, skipping GitHub login.${NC}"
+    echo -e "${RED}[WARNING] - GitHub CLI (gh) not found, skipping GitHub login.${NC}"
 fi
 
-# Bun JS
-echo -e "Post-install: installing BunJS"
-curl -fsSL https://bun.sh/install | bash
+echo -e "${GREEN}[INFO] - Creating bump directory${NC}"
+mkdir -p ~/code/bump
 
-# Deno JS
-echo -e "Post-install: installing DenoJS"
-curl -fsSL https://deno.land/install.sh | sh
-
-# Black Arch
-echo -e "${GREEN}Running Black Arch Bootstrap${NC}"
-curl -o ~/strap.sh https://blackarch.org/strap.sh
-# Verify the SHA1 sum
-echo 26849980b35a42e6e192c6d9ed8c46f0d6d06047 ~/strap.sh | sha1sum -c
-chmod +x ~/strap.sh
-sudo ~/strap.sh
-rm -rf ~/script.sh
-
-# DevKitPro setup
-echo -e "${GREEN}Post-install: Setting up DevKitPro${NC}"
-sudo pacman-key --recv BC26F752D25B92CE272E0F44F7FD5492264BB9D0 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign BC26F752D25B92CE272E0F44F7FD5492264BB9D0
-wget https://pkg.devkitpro.org/devkitpro-keyring.pkg.tar.xz
-sudo pacman -U devkitpro-keyring.pkg.tar.xz
-echo "[dkp-libs]" | sudo tee -a /etc/pacman.conf
-echo "Server = https://pkg.devkitpro.org/packages" | sudo tee -a /etc/pacman.conf
-echo "[dkp-linux]" | sudo tee -a /etc/pacman.conf
-echo "Server = https://pkg.devkitpro.org/packages/linux/\$arch" | sudo tee -a /etc/pacman.conf
-rm -rf devkitpro-keyring.pkg.tar.xz
-
-# System upgrade (optional: upgrade system packages and data)
-echo -e "${GREEN}Post-install: Upgrading system packages${NC}"
-sudo pacman -Syu
-
-sudo systemctl restart systemd-binfmt
-
-# Enable Docker and libvirtd services
-echo -e "${GREEN}Post-install: Enabling Docker and libvirt services${NC}"
-sudo systemctl enable --now docker
-sudo systemctl enable --now libvirtd
+echo -e "${GREEN}[INFO] - Cloning bump repos${NC}"
+echo -e "${GREEN}[INFO] - bump: Cloning x-roulette${NC}"
+git clone https://github.com/itsjustgalileo/x-roulette ~/code/bump/x-roulette
+echo -e "${GREEN}[INFO] - bump: Cloning progen${NC}"
+git clone https://github.com/itsjustgalileo/progen ~/code/bump/progen
+echo -e "${GREEN}[INFO] - bump: Cloning quest${NC}"
+git clone https://github.com/itsjustgalileo/progen ~/code/bump/quest
 
 # Loading emacs config
-echo -e "${GREEN}Post-install: Running emacs to load config${NC}"
-echo -e "${GREEN}Post-install: Wait for emacs to finish loading and close it."
+echo -e "${GREEN}[INFO] - Post-install: Running emacs to load config${NC}"
+echo -e "${GREEN}[INFO] - Post-install: Wait for emacs to finish loading and close it."
 emacs
 
-echo -e "${GREEN}Post-install: Script finished${NC}"
+echo -e "${GREEN}[INFO] - Post-install: Script finished${NC}"
 
 # Clean up
 rm -rf ~/post-install.sh
